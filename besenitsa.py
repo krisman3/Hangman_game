3705bdd041fb8b9ad271500218501579eed5313f
KIliev <kriskokz@gmail.com>
2024-03-07 14:00:12 +0200

    Defined all functions and started creating the main game logic.

--- besenitsa.py
+++ besenitsa.py
@@ -3,12 +3,14 @@ from random import choice
 
 # Setting a random word to guess
 word = ''
-
+def global_count():
+    return 0
 def random_word():
     global word
-    lst_of_words = []
-    word = random.choice(lst_of_words)
-    return word
+    categories = ["biology", "geography", "entertainment", "sports"]
+    category = random.choice(categories)
+    word = input(f"Choose a word from category {category}: ")
+    return word.lower()
 
 
 def print_board(pulled_word):
@@ -18,10 +20,10 @@ def print_board(pulled_word):
 
 def player_guess():
     guess_letter = input("Guess an existing letter: ")
-    return guess_letter
+    return guess_letter.lower()
 
 
-def noose_count(count):
+def noose_count(count):  # I'll use the global_count method for this one
     if count == 0:
         print("--------")
     elif count == 1:
@@ -63,18 +65,26 @@ def noose_count(count):
               "  / \   ")
 
 
-def check_guess(board: list, actual_word: str, player_letter: chr):
+# Once again - will use the global_count method
+def check_guess(board: list, actual_word: str, player_letter: chr, count):
     print(board)
     if player_letter in actual_word:
-        for i in range(len(actual_word)-1):
+        for i in range(len(actual_word)):
             if player_letter == actual_word[i]:
                 board[i] = player_letter
                 continue
-    elif board == actual_word:
+    elif ''.join(board) == actual_word:
+        count = 0
         return 'Congratulations! You survived.'
+
     else:
+        count += 1
+        if count < 8:
+            return "Keep guessing..."
+        else:
+            return "You've been hung."
 
 
-
-    return ''.join(board)
+# Main logic of the game:
+word_to_guess = random_word()
 
